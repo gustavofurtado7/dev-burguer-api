bb61956863ba9d442161613506053859bdd0cd20
gustavofurtado7 <gustavofurtadopeg@gmail.com>
2026-09-23 16:53:42 -0300

    feat: add email existence check in user registration

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -5,6 +5,18 @@ class UserController {
     async store(req, res){
     const {name, email, password_hash, admin} = req.body
 
+
+        const existingUser = await User.findOne({
+            where: {
+                email
+            }
+        })
+
+        if (existingUser) {
+            return res.status(400).json({message: "Este e-mail já está cadastrado!"})
+        }
+
+
      const user = await User.create({
         id: v4(),
         name,
