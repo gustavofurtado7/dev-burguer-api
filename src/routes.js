37ace9884931344890c7a8697fc007117a664c11
gustavofurtado7 <gustavofurtadopeg@gmail.com>
2026-09-24 17:06:00 -0300

    feat: add multer for file uploads and configure product upload route

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import sessionController from './app/controllers/SessionController.js';
 import userController from './app/controllers/UserController.js';
 import ProductController from './app/controllers/ProductController.js';
+import multer from 'multer';
+import multerConfig from './config/multer.cjs';
 
 const routes = new Router();
 
+const upload = multer(multerConfig)
+
 routes.post('/users', userController.store);
 routes.post('/session', sessionController.store);
-routes.post('/products', ProductController.store)
+routes.post('/products', upload.single('file'), ProductController.store)
 
 export default routes;
